fbabe8f68eef0a2f404512906a51cc79e6233ee3
Adelie <dyarob@gmail.com>
2020-09-03 01:22:43 +0200

    doors

--- maps.c
+++ maps.c
@@ -4,7 +4,9 @@ Map	*loadmap(const char *map) {
 
 FILE	*f = fopen(map, "r");
 Map	*m = malloc(sizeof(Map));
+int	i = 0;
 
+// info
 fseek(f, 4, 0);
 m->siz[0] = (fgetc(f)-'0')*100;
 m->siz[0] += (fgetc(f)-'0')*10;
@@ -23,13 +25,38 @@ m->mid[1] = (fgetc(f)-'0')*100;
 m->mid[1] += (fgetc(f)-'0')*10;
 m->mid[1] += fgetc(f)-'0';
 
-fgetc(f);
+// doors
+fseek(f, 31, 0);
+while(fgetc(f)!='\n') {
+	i++;
+	fseek(f, 31+18*i, 0);
+}
+m->doors = malloc(sizeof(void*)*i);
+for(int j=0; j<i; j++) {
+	fseek(f, 34+18*j, 0);
+	m->doors[j] = malloc(sizeof(Door));
+	m->doors[j]->dstpath = malloc(7);
+	fread(m->doors[j]->dstpath, 1, 6, f);
+	m->doors[j]->dstpath[6] = '\0';
+	fgetc(f);
+	m->doors[j]->dstpos[0] = (fgetc(f)-'0')*100;
+	m->doors[j]->dstpos[0] += (fgetc(f)-'0')*10;
+	m->doors[j]->dstpos[0] += fgetc(f)-'0';
+	fgetc(f);
+	m->doors[j]->dstpos[1] = (fgetc(f)-'0')*100;
+	m->doors[j]->dstpos[1] += (fgetc(f)-'0')*10;
+	m->doors[j]->dstpos[1] += fgetc(f)-'0';
+}
+fseek(f, 32+18*i, 0);
+
+// actual map
 m->s = malloc(m->siz[0]*(m->siz[1]+1));
 fread(m->s, 1, m->siz[0]*(m->siz[1]+1), f);
 
 fclose(f);
 
-// collision map
+
+// collision map generation
 m->collision = malloc(m->siz[0]*m->siz[1]);
 for(int j=0; j<m->siz[0]; j++)
 for(int i=0; i<m->siz[1]; i++)
--- maps.h
+++ maps.h
@@ -5,11 +5,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef struct {
+	char	*dstpath;
+	int	dstpos[2];
+} Door;
+
 typedef struct	{
 	int	siz[2];
 	int	mid[2];
 	char	*s;
 	bool	*collision;
+	Door	**doors;
 } Map;
 
 Map	*loadmap(const char *map);
