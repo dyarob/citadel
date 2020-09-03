7981b714886d866b567a71128e466efc1db41f0c
Adelie <dyarob@gmail.com>
2020-09-03 03:06:56 +0200

    doorways operational

--- main.c
+++ main.c
@@ -15,10 +15,9 @@ refresh();
 char	c = 0;
 //current map
 WINDOW	*mapw = newwin(LINES, COLS, 0, 0);
-int	mapoffset[2] = {0, 0};
 Map	*map = loadmap("maps/1");
 //character
-int	cpos[2] = {map->mid[0]-1, map->mid[1]-1};
+int	cpos[2] = {10, 18};
 //UI
 WINDOW	*mtitl = newwin(5, 25, 1, 2);
 
@@ -27,13 +26,11 @@ while(1) {
 	//map
 	werase(mapw);
 	for(int i=0; i<map->siz[0]; i++)
-		mvwaddnstr(mapw, LINES/2-map->mid[0] + i + mapoffset[0],
-			(COLS/2-map->mid[1]+mapoffset[1]>=0?COLS/2-map->mid[1]+mapoffset[1]:0),
-			map->s + i*(map->siz[1]+1)
-			-(COLS/2-map->mid[1]+mapoffset[1]<0?COLS/2-map->mid[1]+mapoffset[1]:0),
-			(COLS/2-map->mid[1]+mapoffset[1]+map->siz[1]<COLS?map->siz[1]:map->siz[1]+(COLS-(COLS/2-map->mid[1]+mapoffset[1]+map->siz[1]))));
+		mvwaddnstr(mapw, LINES/2-cpos[0] + i, (COLS/2-cpos[1]>=0?COLS/2-cpos[1]:0),
+			map->s + i*(map->siz[1]+1) -(COLS/2-cpos[1]<0?COLS/2-cpos[1]:0),
+			(COLS/2-cpos[1]+map->siz[1]<COLS?map->siz[1]:map->siz[1]+(COLS-(COLS/2-cpos[1]+map->siz[1]))));
 	//character
-	mvaddch(LINES/2-1, COLS/2-1, 'C');
+	mvaddch(LINES/2, COLS/2, 'C');
 	wrefresh(mapw);
 	//UI
 	box(mtitl, 0, 0);
@@ -44,20 +41,16 @@ while(1) {
 	while((c=getch())==ERR);
 	if(c == 'q') break;
 	//movement
-	else if(c == 'e' && !map->collision[(cpos[0]-1)*map->siz[1]+cpos[1]])
-		{ mapoffset[0]++; cpos[0]--; }
-	else if(c == 'd' && !map->collision[(cpos[0]+1)*map->siz[1]+cpos[1]])
-		{ mapoffset[0]--; cpos[0]++; }
-	else if(c == 's' && !map->collision[(cpos[0])*map->siz[1]+cpos[1]-1])
-		{ mapoffset[1]++; cpos[1]--; }
-	else if(c == 'f' && !map->collision[(cpos[0])*map->siz[1]+cpos[1]+1])
-		{ mapoffset[1]--; cpos[1]++; }
+	else if(c == 'e' && !map->collision[(cpos[0]-1)*map->siz[1]+cpos[1]]) cpos[0]--;
+	else if(c == 'd' && !map->collision[(cpos[0]+1)*map->siz[1]+cpos[1]]) cpos[0]++;
+	else if(c == 's' && !map->collision[(cpos[0])*map->siz[1]+cpos[1]-1]) cpos[1]--;
+	else if(c == 'f' && !map->collision[(cpos[0])*map->siz[1]+cpos[1]+1]) cpos[1]++;
+	//going through doors
 	if(map->s[cpos[0]*(map->siz[1]+1)+cpos[1]] == 'D') {
 		int i;
-		for(i=0; map->doors[i]->pos[0]!=cpos[0] && map->doors[i]->pos[1]!=cpos[1]; i++);
+		for(i=0; map->doors[i]->pos[0]!=cpos[0] || map->doors[i]->pos[1]!=cpos[1]; i++);
+		cpos[0] = map->doors[i]->dstpos[0]; cpos[1] = map->doors[i]->dstpos[1];
 		map = loadmap(map->doors[i]->dstpath);
-		cpos[0] = map->mid[0]-1; cpos[1] = map->mid[1]-1;
-		mapoffset[0] = 0; mapoffset[1] = 0;
 	}
 }
 
